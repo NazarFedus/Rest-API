56cc21b5600479fee043012f5718bc656b4dbe1d
NazarFedus <simpleprograming7@gmail.com>
2022-12-29 14:49:02 +0000

    establish work

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+app.use(express.static(path.resolve(__dirname, 'client')));
+
+// to handle all the routes
+app.get('*', (req, res) => {
+     res.sendFile(path.resolve(__dirname, 'client', 'index.html')); // send the index.html file
+})
+
+app.listen(3000, () => {
+     console.log('Server is running on port 3000...');
+})
