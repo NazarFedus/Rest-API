712bd281f9fd634545e959ec8c0a3e8c2f77c2f2
NazarFedus <simpleprograming7@gmail.com>
2022-12-30 14:18:02 +0000

    client part has done

--- client/frontend.js
+++ client/frontend.js
@@ -7,12 +7,31 @@ new Vue({
                form: {
                     name: '',
                     value: '',
-               }
+               },
+               contacts: [
+                    {id: 1, name: 'Nazar', value: '+1234567890', marked: false},
+               ]
+          }
+     },
+     computed: {
+          canCreate(){
+               return this.form.name.trim() && this.form.value.trim()
           }
      },
      methods: {
           createContact(){
-               console.log('Create contact', this.form);
+               const {...contact} = this.form
+
+               this.contacts.push({...contact, id: Date.now(), marked: false})
+
+               this.form.name = this.form.value = '' // clear the form
+          },
+          markContact(id){
+               const contact = this.contacts.find(a => a.id === id);
+               contact.marked = true;
+          },
+          removeContact(id){
+               this.contacts = this.contacts.filter(a => a.id !== id)
           }
      }
 })
